12e4e862fbe993acb2d06b9eb10bf69338177385
Felipe <Feelipe@email.com>
2019-01-16 01:04:06 -0200

    Desafio [021]

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 const $root = document.querySelector("#root");
 const $MemoryCard = document.createElement("article");
+const $MemoryCardTurned = document.createElement("article");
 
 const $icon = `<img class='icon' 
     src='img/icon-collabcode.png' 
     alt='Mascote CollabCode'>
   </img>`;
 
+const $iconTurned = `<img class='iconTurned' 
+    src='img/icon-js.png' 
+    alt='Mascote CollabCode'>
+  </img>`;
+
 $MemoryCard.classList.add("memory-card");
+$MemoryCardTurned.classList.add("memory-card-turned");
+
+$root.insertBefore($MemoryCardTurned, null);
+$MemoryCardTurned.insertAdjacentHTML("afterbegin", $iconTurned);
 
 $root.insertBefore($MemoryCard, null);
 $MemoryCard.insertAdjacentHTML("afterbegin", $icon);
