6bf061f76bbd8d57e4e926625396399b1b566fb5
Felipe <Feelipe@email.com>
2019-01-18 17:05:02 -0200

    Desafio Único [023]

--- src/pages/game/index.js
+++ src/pages/game/index.js
@@ -3,3 +3,12 @@ createMemoryCardFront();
 for (let card = 1; card < 8; card++) {
   createMemoryCard();
 }
+
+const $cards = document.querySelectorAll(".memory-card");
+
+$cards.forEach(($card, key) => {
+  $card.addEventListener("click", event => {
+    event.preventDefault();
+    console.log("Funcionando", key);
+  });
+});
