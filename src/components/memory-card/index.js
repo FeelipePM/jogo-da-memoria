224e3d10d7db0d3118684de25475dfa697154a61
Felipe <Feelipe@email.com>
2019-01-24 16:38:20 -0200

    Desafio da aula [026]

--- src/components/memory-card/index.js
+++ src/components/memory-card/index.js
@@ -9,3 +9,13 @@ const createMemoryCard = ({ src, alt, nameClass }) =>
     `;
 
 const handleClick = () => console.log("Funcionando");
+
+function createCss() {
+  const $head = document.querySelector("head");
+  const $style = document.createElement("link");
+  $style.rel = "stylesheet";
+  $style.href = "src/components/memory-card/styles.css";
+  $head.insertBefore($style, null);
+}
+
+createCss();
