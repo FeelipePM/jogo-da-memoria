bf5addbae9073f3f8d9853b05dcbf2145207e4cc
Felipe Machado <feelipe@email.com>
2019-03-12 11:22:52 -0300

    Desafio Intermediário [038]

--- src/pages/signup/index.js
+++ src/pages/signup/index.js
@@ -1,8 +1,8 @@
 (function() {
   const $root = document.querySelector("#root");
 
-  const $flatButton = flatButton.render();
-  const $flatButtonSignUp = flatButton.render();
+  const $flatButton = flatButton.render("Log in");
+  const $flatButtonSignUp = flatButton.render("Sign up");
 
   $root.insertAdjacentHTML("beforeend", $flatButton);
   $root.insertAdjacentHTML("beforeend", $flatButtonSignUp);
