8e787c8aaebc9253178485b05f610013b7df5d15
Felipe Machado <feelipe@email.com>
2019-03-14 17:03:39 -0300

    Desafio Básico [038]

--- src/pages/signup/index.js
+++ src/pages/signup/index.js
@@ -3,7 +3,9 @@
 
   const $loginButton = flatButton.render("Log in");
   const $signUpButton = flatButton.render("Sign up", true);
+  const $iconCollab = iconCollab.render();
 
   $root.insertAdjacentHTML("beforeend", $loginButton);
   $root.insertAdjacentHTML("beforeend", $signUpButton);
+  $root.insertAdjacentHTML("beforeend", $iconCollab);
 })();
