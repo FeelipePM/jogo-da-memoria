16e0ac119471b031cd2d36c28d1122aa1d8623fa
Felipe Machado <feelipe@email.com>
2019-03-26 09:42:32 -0300

    Desafio Básico [041]

--- src/pages/signup/index.js
+++ src/pages/signup/index.js
@@ -6,14 +6,28 @@
 
   const $logoCollabCode = logoCollabCode.render();
   const $titleCollabCode = titleCollabCode.render("Welcome!");
+  const $labelNameCollabCode = labelCollabCode.render("Username");
+  const $inputNameCollabCode = inputCollabCode.render("name-example");
   const $labelCollabCode = labelCollabCode.render("E-mail");
   const $inputCollabCode = inputCollabCode.render("example@email.com");
+  const $labelPasswordCollabCode = labelCollabCode.render("Password");
+  const $inputPasswordCollabCode = inputCollabCode.render("your password");
+  const $labelPassConfirmCollabCode = labelCollabCode.render("Cofirm Password");
+  const $inputPassConfirmCollabCode = inputCollabCode.render(
+    "corfirm your password"
+  );
 
   const $logoWrapper = logoWrapper.render($logoCollabCode, $titleCollabCode);
 
   $root.insertAdjacentHTML("beforeend", $loginButton);
   $root.insertAdjacentHTML("beforeend", $signUpButton);
   $root.insertAdjacentHTML("beforeend", $logoWrapper);
+  $root.insertAdjacentHTML("beforeend", $labelNameCollabCode);
+  $root.insertAdjacentHTML("beforeend", $inputNameCollabCode);
   $root.insertAdjacentHTML("beforeend", $labelCollabCode);
   $root.insertAdjacentHTML("beforeend", $inputCollabCode);
+  $root.insertAdjacentHTML("beforeend", $labelPasswordCollabCode);
+  $root.insertAdjacentHTML("beforeend", $inputPasswordCollabCode);
+  $root.insertAdjacentHTML("beforeend", $labelPassConfirmCollabCode);
+  $root.insertAdjacentHTML("beforeend", $inputPassConfirmCollabCode);
 })();
