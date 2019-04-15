7eade8ced4ad867acdb63e1bd8abeb055a20388d
Felipe Machado <feelipe@email.com>
2019-04-15 17:09:27 -0300

    CSS do link-collab

--- src/objects/link-collab/index.js
+++ src/objects/link-collab/index.js
@@ -1,13 +1,31 @@
 const linkCollab = (function() {
   const module = {};
 
-  module._style = () => {};
+  module._style = () => {
+    const $head = document.querySelector("head");
+    const $style = document.createElement("style");
+
+    $style.textContent = `
+      .link-collab {
+        display: block;
+        text-align: right;
+        color: #3a4042;
+        font-size: 14px;
+        opacity: 0.7;
+        text-decoration: none;
+        margin-top: 40px;
+        margin-bottom: 60px
+      }
+    `;
+
+    $head.insertAdjacentElement("beforeend", $style);
+  };
 
   module.render = ({ href, content }) => {
     module._style();
 
     return `
-      <a href="${href}">${content}</a>
+      <a class="link-collab" href="${href}">${content}</a>
     `;
   };
 
